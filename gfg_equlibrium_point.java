9503af5bde2a92df23d780fc584450d9dd2234f5
Aryan Gautam <100340841+aryangtm@users.noreply.github.com>
2023-01-10 23:43:43 +0530

    Create gfg_equlibrium_point.java

---
+++ gfg_equlibrium_point.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    
+    // a: input array
+    // n: size of array
+    // Function to find equilibrium point in the array.
+    public static int equilibriumPoint(long a[], int n) {
+
+        long s1=0,s2=0;
+        for(int i = 0;i<n;i++)s1=s1+a[i];
+        for(int i = 0;i<n;i++){
+            s1=s1-a[i];
+            if(s1==s2){
+                return i+1;
+            }
+            else{
+                s2=s2+a[i];
+            }
+        }
+        return -1;
+    }
+}
