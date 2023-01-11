61e70e1aedd5849c4347da109242209aa3b9d1a4
Aryan Gautam <100340841+aryangtm@users.noreply.github.com>
2023-01-12 01:39:37 +0530

    Create gfg_majority_vote.java

---
+++ gfg_majority_vote.java
@@ -0,0 +1,28 @@
+class Solution {
+    public static ArrayList<Integer> Solve(int x, int[] a) {
+        ArrayList<Integer> al = new ArrayList<>();
+        int  c=0;
+        int ch = x/3;
+        for (int j = 0; j < x; j++) {
+            c=0;
+            for (int k = j+1; k < x ; k++) {
+                if(a[j]==a[k]){
+                    c++;
+                }
+            }
+            if(c>=x/3){
+                if(!al.contains(a[j])){
+                    al.add(a[j]);
+                }
+            }
+        }
+        boolean cha = al.isEmpty();
+        if(cha){
+            al.add(-1);
+            return al;
+        }
+        else{
+            return al;
+        }
+    }
+}
