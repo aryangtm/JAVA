1ed1561e9b357f0fb04a709aed8523a72e22924e
Aryan Gautam <100340841+aryangtm@users.noreply.github.com>
2023-01-15 22:31:05 +0530

    Create lc_1365.java

---
+++ lc_1365.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] an = new int[nums.length];
+        for(int i = 0;i<nums.length;i++){
+            int c =0;
+            for(int j = 0;j<nums.length;j++){
+                if(nums[i]>nums[j]){
+                    c++;   
+                }
+                an[i]=c;
+            }
+        }
+        return an;
+    }
+}
