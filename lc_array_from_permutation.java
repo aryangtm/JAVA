e9ddb28b0b812ce022b2fa4cbc66a4a8fbaa253e
Aryan Gautam <100340841+aryangtm@users.noreply.github.com>
2023-01-22 22:32:00 +0530

    Update lc_array_from_permutation.java

--- lc_array_from_permutation.java
+++ lc_array_from_permutation.java
@@ -1,9 +1,9 @@
 class Solution {
     public int[] buildArray(int[] nums) {
-    int[] a =new int[nums.length];
+    int[] as =new int[nums.length];
     for(int i =0;i<nums.length;i++){
-        a[i]=nums[nums[i]];
+        as[i]=nums[nums[i]];
     }
-    return a;
+    return as;
     }
 }
