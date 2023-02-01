b356a38d63212262d27c5214fd0a25f2ff00cfb9
Aryan Gautam <100340841+aryangtm@users.noreply.github.com>
2023-02-01 14:28:47 +0530

    Create lc_matrix_diagonal_sum.java

---
+++ lc_matrix_diagonal_sum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int diagonalSum(int[][] a) {
+        int s =0;
+        for(int i =0;i<a.length;i++){
+            for(int j =0;j<a[i].length;j++){
+                if(i==j||i+j==a.length-1){
+                    s = s+a[i][j];
+                }
+            }
+        }       
+        return s;
+    }
+}
