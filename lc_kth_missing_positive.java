afc172b91932b7e474bd44bbde4ee839ef7461a0
Aryan <100340841+aryangtm@users.noreply.github.com>
2023-03-06 11:21:20 +0530

    Create lc_kth_missing_positive.java

---
+++ lc_kth_missing_positive.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int l = 0, r = arr.length - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (arr[mid] - mid - 1 < k) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return l + k;
+    }
+}
