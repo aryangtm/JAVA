7e22a99f01fa8303973734d147a678775c8136b4
Aryan <100340841+aryangtm@users.noreply.github.com>
2023-03-15 23:24:54 +0530

    Update insertion_java.java

--- insertion_java.java
+++ insertion_java.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 class X{
     public static void main(String[] args) {
-        Scanner i = new Scanner(System.in);
-        int n = i.nextInt();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
         int[] arr = new int[n+1];
         System.out.println("Enter element you want to insert in the array : ");
-        int x = i.nextInt();
+        int x = in.nextInt();
         System.out.println("Enter index you want to insert element in : ");
-        int idx = i.nextInt();
+        int idx = in.nextInt();
         for (int j = 0; j < n; j++) {
-            arr[j] = i.nextInt();
+            arr[j] = in.nextInt();
         }
         for (int k = n-1; k >=idx ; k--) {
             arr[k+1] = arr[k];
