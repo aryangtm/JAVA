d43807e33e5a346a7b63e687031657e7f4302ab8
Aryan <100340841+aryangtm@users.noreply.github.com>
2023-03-17 22:22:51 +0530

    Create lc_number_of_steps_to_reduce_a_number_to_zero.java

---
+++ lc_number_of_steps_to_reduce_a_number_to_zero.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numberOfSteps(int n) {
+        int c = 0;
+        while(n>0){
+        if(n%2==0){
+            n=n/2;
+            c++;
+        }
+        else if(n%2!=0){
+            n=n-1;
+            c++;
+        }
+        }
+        return c;
+
+    }
+}
