7b2b502da68808395a1a9440fec87b0e4ff72780
Aryan <100340841+aryangtm@users.noreply.github.com>
2023-04-01 02:56:56 +0530

    Create lc_remove_linked_list_elements.java

---
+++ lc_remove_linked_list_elements.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) {
+            return null;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode curr = head;
+        while (curr != null) {
+            if (curr.val == val) {
+                prev.next = curr.next;
+            } else {
+                prev = curr;
+            }
+            curr = curr.next;
+        }
+        
+        return dummy.next;
+    }
+}
