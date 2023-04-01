f5f7bdda9ac08816c853ea8b7ca4b00c647a6234
Aryan <100340841+aryangtm@users.noreply.github.com>
2023-04-01 16:47:57 +0530

    Update lc_contains_duplicate.java

--- lc_contains_duplicate.java
+++ lc_contains_duplicate.java
@@ -1,11 +1,9 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i =1;i<nums.length;i++){
-            if(nums[i-1]==nums[i]){
-                return true;
-            }
+        HashSet<Integer> s = new HashSet<>();
+        for(int i = 0;i<nums.length;i++){
+            s.add(nums[i]);
         }
-        return false;
+        return nums.length!=s.size();
     }
 }
