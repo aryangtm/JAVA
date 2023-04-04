2df430a82f7f77a975a34f24e1533ad2988e6084
Aryan <100340841+aryangtm@users.noreply.github.com>
2023-04-04 23:21:04 +0530

    Update lc_2011.java

--- lc_2011.java
+++ lc_2011.java
@@ -1,13 +1,13 @@
 class Solution {
     public int finalValueAfterOperations(String[] a) {
-        int x =0;
+        int mx =0;
         for(int i=0;i<a.length;i++){
             if(a[i].charAt(1)=='-'){
-                --x;
+                --mx;
             }else{
-                x++;
+                mx++;
             }
         }
-        return x;
+        return mx;
     }
 }
