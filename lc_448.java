9a2c0c9781c0444de8df4dbdb76c551f58cb2fe8
Aryan <100340841+aryangtm@users.noreply.github.com>
2023-04-17 23:37:58 +0530

    Update lc_448.java

--- lc_448.java
+++ lc_448.java
@@ -1,12 +1,12 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] a) {
-        HashSet<Integer> s = new HashSet<Integer>();
+        HashSet<Integer> si = new HashSet<Integer>();
         List<Integer> ans = new ArrayList<>();
         for(int i = 0;i<a.length;i++){
-            s.add(a[i]);
+            si.add(a[i]);
         }
         for(int i =1;i<=a.length;i++){
-            if(!s.contains(i)){
+            if(!si.contains(i)){
                 ans.add(i);
             }
         }
