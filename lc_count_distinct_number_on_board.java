1d8169cfc8e3f27dd70ad3d549fe31d2d0f68c9e
Aryan <100340841+aryangtm@users.noreply.github.com>
2023-04-26 23:41:03 +0530

    Update lc_count_distinct_number_on_board.java

--- lc_count_distinct_number_on_board.java
+++ lc_count_distinct_number_on_board.java
@@ -1,7 +1,7 @@
 class Solution {
-    public int distinctIntegers(int n) {
-        if(n>=2) return n-1;
-        else return n;
+    public int distinctIntegers(int ni) {
+        if(ni>=2) return ni-1;
+        else return ni;
 
     }
 }
