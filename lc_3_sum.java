c26609c9a88f5e4ec52268ad76daa15fc12a77d6
Aryan <100340841+aryangtm@users.noreply.github.com>
2023-05-24 23:52:33 +0530

    Create lc_3_sum.java

---
+++ lc_3_sum.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new LinkedList<List<Integer>>();
+
+    for(int i =0;i<nums.length;i++){
+        if(i>0 && nums[i]==nums[i-1]) continue;
+        int j = i+1;
+        int k = nums.length-1;
+        while(j<k){
+            int sum=nums[i]+nums[j]+nums[k];
+            if(sum<0)j++;
+            else if(sum>0)k--;
+            else{
+                ArrayList<Integer> a = new ArrayList<Integer>();
+                a.add(nums[i]);
+                a.add(nums[j]);
+                a.add(nums[k]);
+                ans.add(a);
+                j++;
+                k--;
+                while(j<k && nums[j]==nums[j-1])j++;
+                while(j<k && nums[k]==nums[k+1])k--;
+            }
+           
+        }
+         
+    }
+    return ans;
+}
+}
