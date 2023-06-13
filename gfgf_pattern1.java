9c25e7923c118d7bc32839c36fe242d39bec68c6
Aryan <100340841+aryangtm@users.noreply.github.com>
2023-06-13 18:20:29 +0530

    Create gfgf_pattern1.java

---
+++ gfgf_pattern1.java
@@ -0,0 +1,12 @@
+
+class Solution {
+
+    void printSquare(int n) {
+        for(int i = 0;i<n;i++){
+            for(int j =0;j<n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
