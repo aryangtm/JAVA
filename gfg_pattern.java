b728f320c53c6ba74a03d9e1edd12ce376287e44
Aryan <100340841+aryangtm@users.noreply.github.com>
2023-06-13 22:51:38 +0530

    Create gfg_pattern.java

---
+++ gfg_pattern.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    void printDiamond(int n) {
+                for(int i =0;i<n;i++){
+            for(int j =0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int j =0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i = n-1;i>=0;i--){
+            for(int  j = 0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int j = 0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
