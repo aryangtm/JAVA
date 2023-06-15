ed29fcffbd1d70be1ca0ca4df09ba0a1b96bfc7c
Aryan <100340841+aryangtm@users.noreply.github.com>
2023-06-16 00:28:48 +0530

    Create gfg_pattern14.java

---
+++ gfg_pattern14.java
@@ -0,0 +1,14 @@
+
+class Solution {
+
+    void printTriangle(int n) {
+        for(int i = 0;i<n;i++)
+       {
+        for(char c = 'A';c<='A'+i;c++){
+            System.out.print(c);
+            
+        }   
+        System.out.println();
+    }
+}
+}
