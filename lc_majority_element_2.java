27ff0ee38d346bd1aed13bf876e7cf539c93fd49
Aryan <100340841+aryangtm@users.noreply.github.com>
2023-10-06 00:43:36 +0530

    Update lc_majority_element_2.java

--- lc_majority_element_2.java
+++ lc_majority_element_2.java
@@ -2,15 +2,29 @@ class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int checker = (int)(nums.length/3)+1;
         List<Integer> ans= new LinkedList<Integer>();
-        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
-        for(int i= 0;i<nums.length;i++){
-            int value = map.getOrDefault(nums[i],0);
-            map.put(nums[i],value+1);
-            if(map.get(nums[i])==checker){
-                ans.add(nums[i]);
+        int c1 = 0,e1=0,c2=0,e2=0,count1 =0,count2=0;
+        for(int i = 0;i<nums.length;i++){
+            if(c1==0 && nums[i]!=e2){
+                c1 = 1;
+                e1 = nums[i];
             }
-            //if(ans.size()==checker)break;
+            else if( c2 == 0 && nums[i] != e1){
+                c2 = 1;
+                e2 = nums[i];
+            }
+            else if(e1==nums[i])c1++;
+            else if(e2==nums[i])c2++;
+            else{
+                c1--;
+                c2--;
+            }
         }
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i]==e1)count1++;
+            else if(nums[i]==e2)count2++;
+        }
+        if(count1>=checker)ans.add(e1);
+        if(count2>=checker)ans.add(e2);
         return ans;
     }
 }
