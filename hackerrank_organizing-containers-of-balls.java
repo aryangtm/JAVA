c9992d9e96f440e7762bf60b56a65bd781c48037
Aryan <100340841+aryangtm@users.noreply.github.com>
2025-02-13 15:13:45 +0530

    Create hackerrank_organizing-containers-of-balls.java

---
+++ hackerrank_organizing-containers-of-balls.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'organizingContainers' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts 2D_INTEGER_ARRAY container as parameter.
+     */
+
+    public static String organizingContainers(List<List<Integer>> container) {
+    int n = container.size(); 
+    long[] rowSum = new long[n];
+    long[] colSum = new long[n];
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j<n;j++){
+            int balls = container.get(i).get(j);
+            colSum[i]+=balls;
+            rowSum[j]+=balls;
+        }
+    }
+    Arrays.sort(colSum);
+    Arrays.sort(rowSum);
+    for(int i = 0;i<n;i++){
+        if(rowSum[i]!=colSum[i]){
+            return "Impossible";
+        }
+    }
+    return "Possible";
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<List<Integer>> container = new ArrayList<>();
+
+                IntStream.range(0, n).forEach(i -> {
+                    try {
+                        container.add(
+                            Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                        );
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                });
+
+                String result = Result.organizingContainers(container);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
